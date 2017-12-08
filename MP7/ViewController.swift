34ca97f0aea422e4effb3b5e9c9415dda6a2ad7c
Taha Syed <Taha@wirelessprv-10-194-203-194.near.illinois.edu>
2017-12-08 17:07:11 -0600

    git

--- MP7/ViewController.swift
+++ MP7/ViewController.swift
@@ -7,19 +7,52 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    //Create and array of sounds.
+    let soundFileNames = ["fired", "news", "wall", "rich", "wrong", "china"]
+    
+    //Create an array of audio players.
+    var audioPlayers = [AVAudioPlayer]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
+    
+        //Setup audio players
+        for sound in soundFileNames {
+            
+            do {
+               
+                let url = NSURL(fileURLWithPath: Bundle.main.path(forResource: sound, ofType: ".mp3")!);
+                let audioPlayer = try AVAudioPlayer(contentsOf: url as URL);
+                
+                audioPlayers.append(audioPlayer)
+                
+            } catch {
+                
+                audioPlayers.append(AVAudioPlayer())
+                
+            }
+        }
 
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
-
-
 }
-
+   
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        
+        //Get the audio player for the corresponding button that was tapped.
+        let audioPlayer = audioPlayers[sender.tag]
+        audioPlayer.play()
+        
+        
+    }
+    
+    
+    
+}
